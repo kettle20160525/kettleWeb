4bb4de0f3360d30600e27f6d096de16c247b8929
b309lab <489381688@qq.com>
2016-06-13 16:49:18 +0800

    测试-FTP上传

--- kettle/kettle-ext/src/main/resources/org/flhy/ext/job/steps/FTP_PUT.js
+++ kettle/kettle-ext/src/main/resources/org/flhy/ext/job/steps/FTP_PUT.js
@@ -1,254 +1,155 @@
-JobEntryFTP_PUT = Ext.extend(Ext.Window, {
-	title: 'FTP上传',
+JobEntryFTPPUTDialog = Ext.extend(KettleTabDialog, {
 	width: 700,
 	height: 550,
-	closeAction: 'close',
-	modal: true,
-	layout: 'border',
+	title: 'FTP上传',
 	initComponent: function() {
-		var me = this,
-		graph = getActiveGraph().getGraph(), 
-		cell = graph.getSelectionCell();
+		var me = this,  graph = getActiveGraph().getGraph(),  cell = graph.getSelectionCell();
 		
-		var form = new Ext.form.FormPanel({
-			bodyStyle: 'padding: 15px',
-			defaultType: 'textfield',
-			border: false,
-			labelWidth: 100,
-			region: 'north',
-			height: 50,
-			labelAlign: 'right',
-			items: [{
-				fieldLabel: '步骤名称',
-				anchor: '-10',
-				name: 'label',
-				value: cell.getAttribute('label')
-			}]
+		var wServerName = new Ext.form.TextField({fieldLabel: 'FTP服务器名称/IP地址', anchor: '-10', value: cell.getAttribute('servername')});
+		var wServerPort = new Ext.form.TextField({fieldLabel: '端口', anchor: '-10', value: cell.getAttribute('serverport')});
+		var wUserName = new Ext.form.TextField({fieldLabel: '用户名', anchor: '-10', value: cell.getAttribute('username')});
+		var wPassword = new Ext.form.TextField({fieldLabel: '密码', anchor: '-10', value: cell.getAttribute('password')});
+		var wProxyserver = new Ext.form.TextField({fieldLabel: '代理主机',anchor: '-10', value: cell.getAttribute('proxyserver') });
+		var wProxyserverport = new Ext.form.TextField({fieldLabel: '代理端口',anchor: '-10', value: cell.getAttribute('proxyserverport')});
+		var wProxyserverusername = new Ext.form.TextField({fieldLabel: '代理用户名',anchor: '-10', value: cell.getAttribute('proxyserverusername')});
+		var wProxyserverpwd = new Ext.form.TextField({fieldLabel: '代理密码',anchor: '-10', value: cell.getAttribute('proxyserverpwd')});
+		
+		// 测试连接  一个按钮
+		var wBinaryMode = new Ext.form.Checkbox({fieldLabel: '二进制模式', checked: cell.getAttribute('binarymode') == 'Y'});
+		var wTimeout = new Ext.form.TextField({fieldLabel: '超时',anchor: '-10', value: cell.getAttribute('timeout')});
+		var wUsealiveftpconnection = new Ext.form.Checkbox({fieldLabel: '使用活动的FTP连接', checked: cell.getAttribute('usealiveftpconnection') == 'Y'});
+		var wControlEncode = new Ext.form.ComboBox({
+			fieldLabel: '控制编码',
+			anchor: '-10',
+			displayField: 'name',
+			valueField: 'name',
+			typeAhead: true,
+	        forceSelection: true,
+	        triggerAction: 'all',
+	        selectOnFocus:true,
+			store: Ext.StoreMgr.get('contrlEncodeStore'),
+			value: cell.getAttribute('contrlEncode')
 		});
-
-		var transset = new Ext.form.FormPanel({
+		
+		
+		var wTongpeifu = new Ext.form.TextField({fieldLabel: '通配符', anchor: '-10', value: cell.getAttribute('tongpeifu')});
+		var wDellocalfileAfterupload = new Ext.form.Checkbox({fieldLabel: '上传文件后删除本地文件', checked: cell.getAttribute('dellocalfilesafterupload')=='Y'  });
+		var wNotcoverremotefiles= new Ext.form.Checkbox({fieldLabel: '不覆盖文件', checked: cell.getAttribute('notcoverremotefiles')=='Y'  });
+		
+		var wProxy2server = new Ext.form.TextField({fieldLabel: '主机', anchor: '-10', value: cell.getAttribute('proxy2server')});
+		var wProxy2serverport = new Ext.form.TextField({fieldLabel: '端口', anchor: '-10', value: cell.getAttribute('proxy2serverport')});
+		var wProxy2serverusername = new Ext.form.TextField({fieldLabel: '用户名', anchor: '-10', value: cell.getAttribute('proxy2serverusername')});
+		var wProxy2serverpwd = new Ext.form.TextField({fieldLabel: '密码', anchor: '-10', value: cell.getAttribute('proxy2serverpwd')});
+		
+		this.getValues = function(){
+			return {
+				servername: wServerName.getValue(),
+				serverport: wServerPort.getValue(),
+				username: wUserName.getValue(),
+				password: wPassword.getValue(),
+				proxyserver: wProxyserver.getValue() ,
+				proxyserverport: wProxyserverport.getValue(),
+				proxyserverusername: wProxyserverusername.getValue(),
+				proxyserverpwd: wProxyserverpwd.getValue(),
+				
+				binarymode:wBinaryMode.getValue()?"Y":"N",
+				timeout: wTimeout.getValue(),
+				useraliveftpconnection: wUsealiveftpconnection.getValue()?"Y":"N",
+				controlencode: wControlEncode.getValue(),
+				
+				tongpeifu: wTongpeifu.getValue(),
+				dellocalfileafterupload: wDellocalfileAfterupload.getValue()?"Y":"N",
+				notcoverremotefiles: wNotcoverremotefiles.getValue()?"Y":"N",
+				proxypassword: wProxyPassword.getValue(),
+				
+				proxy2server: wProxy2server.getValue(),
+				proxy2serverport: wProxy2serverport.getValue(),
+				proxy2serverusername: wProxy2serverusername.getValue(),
+				proxy2serverpwd : wProxy2serverpwd.getValue()
+			};
+		};
+		
+		this.tabItems = [
+		                 
+		{
+			xtype: 'KettleForm',
 			title: '一般',
-			bodyStyle: 'padding: 20px 10px',
-			labelWidth: 150,
-			labelAlign: 'right',
-			defaultType: 'textfield',
+			bodyStyle: 'padding: 10px 10px',
+			labelWidth: 130,
 			items: [{
-				fieldLabel: 'FTP服务器名称/IP地址',
-				anchor: '-10',
-				name: 'ftpnameandipaddress',
-				value: cell.getAttribute('partitioning_field')
+				xtype: 'fieldset',
+				title: '服务器设置',
+				items: [wServerName, wServerPort, wUserName, wPassword,wProxyserver, 
+				        wProxyserverport,  wProxyserverusername, wProxyserverpwd,{
+								xtype: 'compositefield',
+								fieldLabel: '测试',
+								anchor: '-10',
+								items: [ {
+									xtype: 'button', text: '测试连接'
+								}]
+							}
+				]
+			},{
+				xtype: 'fieldset',
+				title: '高级设置',
+				items: [ wBinaryMode, wTimeout, wUsealiveftpconnection,wProxyType ]
+			}]
+		},
+		
+		
+		
+		
+		{
+			xtype: 'KettleForm',
+			title: '文件',
+			bodyStyle: 'padding: 10px 10px',
+			labelWidth: 200,
+			items: [{
+				xtype: 'fieldset',
+				title: '源(本地)文件',
+				items: [{
+					xtype: 'compositefield',
+					fieldLabel: '本地目录',
+					anchor: '-10',
+					items: [{
+						xtype: 'button', text: '浏览...'
+					}]
+				}, wTongpeifu, wDellocalfileAfterupload, wNotcoverremotefiles ]
 			}, 
 			{
-				fieldLabel: '端口',
-				anchor: '-10',
-				name: 'partitioning_field',
-				value: cell.getAttribute('partitioning_field')
-			},
-			{
-				fieldLabel: '用户名',
-				anchor: '-10',
-				name: 'partitioning_field',
-				value: cell.getAttribute('partitioning_field')
-			},
-			{
-				fieldLabel: '密码',
-				anchor: '-10',
-				name: 'partitioning_field',
-				value: cell.getAttribute('partitioning_field')
-			},
-			{
-				fieldLabel: '代理主机',
-				anchor: '-10',
-				name: 'partitioning_field',
-				value: cell.getAttribute('partitioning_field')
-			},
-			{
-				fieldLabel: '代理端口',
-				anchor: '-10',
-				name: 'partitioning_field',
-				value: cell.getAttribute('partitioning_field')
-			},
-			{
-				fieldLabel: '代理用户名',
-				anchor: '-10',
-				name: 'partitioning_field',
-				value: cell.getAttribute('partitioning_field')
-			},
-			{
-				fieldLabel: '代理密码',
-				anchor: '-10',
-				name: 'partitioning_field',
-				value: cell.getAttribute('partitioning_field')
-			},
-			{
-				xtype: 'checkbox',
-				fieldLabel: '二进制模式？',
-				name: 'use_batch',
-				checked: cell.getAttribute('use_batch') == 'Y'
-			},
-			{
-				fieldLabel: '超时',
-				anchor: '-10',
-				name: 'partitioning_field',
-				value: cell.getAttribute('partitioning_field')
-			},
-			{
-				xtype: 'checkbox',
-				fieldLabel: '使用活动的FTP连接？',
-				name: 'use_batch',
-				checked: cell.getAttribute('use_batch') == 'Y'
-			},
-			{
-				xtype: 'textfield',
-				fieldLabel: '控制编码',
-				anchor: '-30',
-				value: cell.getAttribute('shortFileFieldName')
+				xtype: 'fieldset',
+				title: '目标(远程)文件',
+				items: [{
+					xtype: 'compositefield',
+					fieldLabel: '远程目录',
+					anchor: '-10',
+					items: [ {
+						xtype: 'button', text: '测试目录'
+					}]
+				}]
 			}
 			]
-		});
-	 
+		},
 		
-		transset.on('afterrender', function() {
-			var arrays = findItems(transset, 'name', 'specification_method');
-			var rbChecked = function(rb) {
-				var v = rb.inputValue;
-				
-				Ext.each(arrays, function(arrItem) {
-					if(arrItem.getId() == rb.getId()) {
-						arrItem.ownerCt.items.each(function(item) {
-							if(item.getId() != arrItem.getId())
-								item.enable();
-						});
-					} else {
-						arrItem.ownerCt.items.each(function(item) {
-							if(item.getId() != arrItem.getId())
-								item.disable();
-						});
-					}
-				});
-			};
-			
-			var firstChecked = null;
-			Ext.each(arrays, function(radio) {
-				radio.on('check', function(rb, checked) {
-					if(checked == true) rbChecked(rb);
-				});
-				if(radio.inputValue == cell.getAttribute('specification_method'))
-					firstChecked = radio;
-			});
-			if(firstChecked != null)
-				firstChecked.setValue(true);
-		});
 		
-		var advance = new Ext.form.FormPanel({
-			title: '文件',
-			bodyStyle: 'padding: 15px',
-			defaultType: 'textfield',
+		
+		{
+			xtype: 'KettleForm',
+			title: 'Sockets代理',
+			bodyStyle: 'padding: 10px 10px',
 			labelWidth: 200,
-			labelAlign: 'right',
-			items: [
-					{
-						fieldLabel: '本地目录',
-						anchor: '-10',
-						name: 'partitioning_field',
-						value: cell.getAttribute('partitioning_field')
-					},
-					{
-						fieldLabel: '通配符(正则表达式)',
-						anchor: '-10',
-						name: 'partitioning_field',
-						value: cell.getAttribute('partitioning_field')
-					},
-					{
-						xtype: 'checkbox',
-						fieldLabel: '上传文件后删除本地文件？',
-						name: 'use_batch',
-						checked: cell.getAttribute('use_batch') == 'Y'
-					},
-					{
-						xtype: 'checkbox',
-						fieldLabel: '不覆盖本地文件？',
-						name: 'use_batch',
-						checked: cell.getAttribute('use_batch') == 'Y'
-					},
-					{
-						fieldLabel: '远程目录',
-						anchor: '-10',
-						name: 'partitioning_field',
-						value: cell.getAttribute('partitioning_field')
-					}
-			]
-		});
-		
-		var setlog = new Ext.form.FormPanel({
-			title: 'Socks代理',
-			bodyStyle: 'padding: 15px',
-			defaultType: 'textfield',
-			labelWidth: 100,
-			labelAlign: 'right',
 			items: [{
-				xtype: 'textfield',
-				fieldLabel: '主机',
-				name: 'set_logfile',
-				value: cell.getAttribute('set_logfile') 
-			},{
-				xtype: 'textfield',
-				fieldLabel: '端口',
-				name: 'set_append_logfile',
-				value: cell.getAttribute('set_append_logfile') 
-			},{
-				xtype: 'textfield',
-				fieldLabel: '用户名',
-				name: 'set_append_logfile',
-				value: cell.getAttribute('set_append_logfile') 
-			},
-			{
-				xtype: 'textfield',
-				fieldLabel: '密码',
-				name: 'set_append_logfile',
-				value: cell.getAttribute('set_append_logfile') 
+				xtype: 'fieldset',
+				title: '代理',
+				items: [wProxy2server, wProxy2serverport, wProxy2serverusername,wProxy2serverpwd]
 			}
-			 ]
-		});
+			]
+		}
 		
-		var tab = new Ext.TabPanel({
-			activeTab: 0,
-			items: [transset, advance, setlog]
-		});
 		
-		this.items = [form, {
-			region: 'center',
-			border: false,
-			bodyStyle: 'padding: 0 10px 10px 10px',
-			layout: 'fit',
-			items: tab
-		}];
-		
-		var bCancel = new Ext.Button({
-			text: '取消', handler: function() {
-				me.close();
-			}
-		});
-		var bOk = new Ext.Button({
-			text: '确定', handler: function() {
-				graph.getModel().beginUpdate();
-                try
-                {
-                	
-                }
-                finally
-                {
-                    graph.getModel().endUpdate();
-                }
-                
-				me.close();
-			}
-		});
-		
-		this.bbar = ['->', bCancel, bOk];
-		
-		JobEntryFTP_PUT.superclass.initComponent.call(this);
+		];
+		JobEntryFTPPUTDialog.superclass.initComponent.call(this);
 	}
 });
 
-Ext.reg('FTP_PUT', JobEntryFTP_PUT);
+Ext.reg('FTP_PUT', JobEntryFTPPUTDialog);
