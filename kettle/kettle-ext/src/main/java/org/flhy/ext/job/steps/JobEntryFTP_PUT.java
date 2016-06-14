0f5acfd71457f290a62e5998d45a7ee7d85f1469
b309lab <489381688@qq.com>
2016-06-14 11:42:35 +0800

    yin

--- kettle/kettle-ext/src/main/java/org/flhy/ext/job/steps/JobEntryFTP_PUT.java
+++ kettle/kettle-ext/src/main/java/org/flhy/ext/job/steps/JobEntryFTP_PUT.java
@@ -6,7 +6,9 @@ import org.flhy.ext.core.PropsUI;
 import org.flhy.ext.job.step.AbstractJobEntry;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.database.DatabaseMeta;
+import org.pentaho.di.job.entries.ftpput.JobEntryFTPPUT;
 import org.pentaho.di.job.entry.JobEntryInterface;
+import org.pentaho.di.trans.steps.textfileoutput.TextFileOutputMeta;
 import org.pentaho.metastore.api.IMetaStore;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -21,7 +23,12 @@ import com.mxgraph.util.mxUtils;
 public class JobEntryFTP_PUT extends AbstractJobEntry{
 	@Override
 	public void decode(JobEntryInterface jobEntry, mxCell cell, List<DatabaseMeta> databases, IMetaStore metaStore) throws Exception {
-//		org.pentaho.di.job.entries.special.JobEntrySpecial jobEntrySpecial = (org.pentaho.di.job.entries.special.JobEntrySpecial) jobEntry;
+		org.pentaho.di.job.entries.ftpput.JobEntryFTPPUT jobEntryFTPPUT = (org.pentaho.di.job.entries.ftpput.JobEntryFTPPUT) jobEntry;
+		jobEntryFTPPUT.setUserName(cell.getAttribute("username"));
+		jobEntryFTPPUT.setServerPort(cell.getAttribute("serverport"));
+		jobEntryFTPPUT.setPassword(cell.getAttribute("password"));
+		jobEntryFTPPUT.setServerName(cell.getAttribute("servername"));
+		
 //		jobEntrySpecial.setRepeat("Y".equalsIgnoreCase(cell.getAttribute("repeat")));
 //		jobEntrySpecial.setSchedulerType(Const.toInt( cell.getAttribute( "schedulerType" ), 
 //				org.pentaho.di.job.entries.special.JobEntrySpecial.NOSCHEDULING ));
@@ -36,9 +43,21 @@ public class JobEntryFTP_PUT extends AbstractJobEntry{
 
 	@Override
 	public Element encode(JobEntryInterface jobEntry) throws Exception {
-//		org.pentaho.di.job.entries.special.JobEntrySpecial jobEntrySpecial = (org.pentaho.di.job.entries.special.JobEntrySpecial) jobEntry;
+		JobEntryFTPPUT jobEntryFTPPUT = (JobEntryFTPPUT) jobEntry;
+
 		Document doc = mxUtils.createDocument();
 		Element e = doc.createElement(PropsUI.JOB_JOBENTRY_NAME);
+		//一般
+		e.setAttribute("timeout", jobEntryFTPPUT.getTimeout()+"");
+		e.setAttribute("serverport", jobEntryFTPPUT.getServerPort());
+		//控制编码的处理
+		e.setAttribute("contrlEncode",jobEntryFTPPUT.getControlEncoding() );
+		System.out.println("控制编码my"+jobEntryFTPPUT.getControlEncoding());
+		
+		//文件
+		//Sockets处理
+		e.setAttribute("proxy2serverport", jobEntryFTPPUT.getProxyPort());
+		
 		
 //		e.setAttribute("repeat", jobEntrySpecial.isRepeat() ? "Y" : "N");
 //		e.setAttribute("schedulerType", jobEntrySpecial.getSchedulerType() + "");
