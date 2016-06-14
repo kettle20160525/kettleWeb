bc7b1d49413a907bf50127c1542c9fa6bfddc102
b309lab <489381688@qq.com>
2016-06-14 15:05:34 +0800

    yin

--- kettle/kettle-ext/src/main/java/org/flhy/ext/job/steps/JobEntryFTP_PUT.java
+++ kettle/kettle-ext/src/main/java/org/flhy/ext/job/steps/JobEntryFTP_PUT.java
@@ -12,8 +12,11 @@ import org.pentaho.di.trans.steps.textfileoutput.TextFileOutputMeta;
 import org.pentaho.metastore.api.IMetaStore;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+
+import bsh.StringUtil;
 
 import com.mxgraph.model.mxCell;
 import com.mxgraph.util.mxUtils;
@@ -24,21 +27,43 @@ public class JobEntryFTP_PUT extends AbstractJobEntry{
 	@Override
 	public void decode(JobEntryInterface jobEntry, mxCell cell, List<DatabaseMeta> databases, IMetaStore metaStore) throws Exception {
 		org.pentaho.di.job.entries.ftpput.JobEntryFTPPUT jobEntryFTPPUT = (org.pentaho.di.job.entries.ftpput.JobEntryFTPPUT) jobEntry;
+		//一般---服务器设置
 		jobEntryFTPPUT.setUserName(cell.getAttribute("username"));
 		jobEntryFTPPUT.setServerPort(cell.getAttribute("serverport"));
 		jobEntryFTPPUT.setPassword(cell.getAttribute("password"));
 		jobEntryFTPPUT.setServerName(cell.getAttribute("servername"));
+		jobEntryFTPPUT.setProxyHost(cell.getAttribute("proxyserver"));
+		jobEntryFTPPUT.setProxyPort(cell.getAttribute("proxyserverport"));
+		jobEntryFTPPUT.setProxyUsername(cell.getAttribute("proxyserverusername"));
+		jobEntryFTPPUT.setProxyPassword(cell.getAttribute("proxyserverpwd"));
 		
-//		jobEntrySpecial.setRepeat("Y".equalsIgnoreCase(cell.getAttribute("repeat")));
-//		jobEntrySpecial.setSchedulerType(Const.toInt( cell.getAttribute( "schedulerType" ), 
-//				org.pentaho.di.job.entries.special.JobEntrySpecial.NOSCHEDULING ));
-//		
-//		jobEntrySpecial.setIntervalSeconds( Const.toInt( cell.getAttribute( "intervalSeconds" ), 0 ) );
-//		jobEntrySpecial.setIntervalMinutes( Const.toInt( cell.getAttribute( "intervalMinutes" ), 0 ) );
-//		jobEntrySpecial.setHour( Const.toInt( cell.getAttribute( "hour" ), 0 ) );
-//		jobEntrySpecial.setMinutes( Const.toInt( cell.getAttribute( "minutes" ), 0 ) );
-//		jobEntrySpecial.setWeekDay( Const.toInt( cell.getAttribute( "weekDay" ), 0 ) );
-//		jobEntrySpecial.setDayOfMonth( Const.toInt( cell.getAttribute( "dayOfMonth" ), 0 ) );
+		//一般---高级设置
+		jobEntryFTPPUT.setBinaryMode("Y".equals(cell.getAttribute("binarymode") ));
+		String timeoutString = cell.getAttribute("timeout");
+		if( !StringUtils.isEmpty(timeoutString) ){
+			boolean isDigital=false;
+			try{ Integer.parseInt( timeoutString ) ;isDigital=true;}catch(Exception e){}
+			 if( isDigital )
+				 jobEntryFTPPUT.setTimeout( Integer.parseInt( timeoutString ) );
+		}
+		jobEntryFTPPUT.setActiveConnection("Y".equals(cell.getAttribute("useraliveftpconnection")));
+		jobEntryFTPPUT.setControlEncoding(cell.getAttribute("controlencode"));
+		
+		//文件---源（本地）文件
+		jobEntryFTPPUT.setLocalDirectory(cell.getAttribute("localdir"));
+		jobEntryFTPPUT.setWildcard(cell.getAttribute("tongpeifu"));
+		jobEntryFTPPUT.setRemove("Y".equals(cell.getAttribute("dellocalfileafterupload")));
+		jobEntryFTPPUT.setOnlyPuttingNewFiles("Y".equals(cell.getAttribute("notcoverremotefiles")));
+		
+		//文件---目标（远程）文件
+		jobEntryFTPPUT.setRemoteDirectory(cell.getAttribute("remotedir"));
+		
+		//Sockets代理 ---代理
+		jobEntryFTPPUT.setProxyHost(cell.getAttribute("wProxy2server"));
+		jobEntryFTPPUT.setProxyPort(cell.getAttribute("wProxy2serverport"));
+		jobEntryFTPPUT.setProxyUsername(cell.getAttribute("proxy2serverusername"));
+		jobEntryFTPPUT.setProxyPassword(cell.getAttribute("proxy2serverpwd"));
+
 	}
 
 	@Override
@@ -57,16 +82,6 @@ public class JobEntryFTP_PUT extends AbstractJobEntry{
 		//文件
 		//Sockets处理
 		e.setAttribute("proxy2serverport", jobEntryFTPPUT.getProxyPort());
-		
-		
-//		e.setAttribute("repeat", jobEntrySpecial.isRepeat() ? "Y" : "N");
-//		e.setAttribute("schedulerType", jobEntrySpecial.getSchedulerType() + "");
-//		e.setAttribute("intervalSeconds", jobEntrySpecial.getIntervalSeconds() + "");
-//		e.setAttribute("intervalMinutes", jobEntrySpecial.getIntervalMinutes() + "");
-//		e.setAttribute("hour", jobEntrySpecial.getHour() + "");
-//		e.setAttribute("minutes", jobEntrySpecial.getMinutes() + "");
-//		e.setAttribute("weekDay", jobEntrySpecial.getWeekDay() + "");
-//		e.setAttribute("DayOfMonth", jobEntrySpecial.getDayOfMonth() + "");
 		
 		return e;
 	}
