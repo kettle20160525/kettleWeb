15ceb156baeb3684b08f5fec2031b4f16aef9abe
children0001 <1011084402@qq.com>
2016-06-16 10:28:31 +0800

    文件上传报错

--- kettle/kettle-ext/src/main/java/org/flhy/ext/job/steps/JobEntryFTP_PUT.java
+++ kettle/kettle-ext/src/main/java/org/flhy/ext/job/steps/JobEntryFTP_PUT.java
@@ -6,17 +6,14 @@ import org.flhy.ext.core.PropsUI;
 import org.flhy.ext.job.step.AbstractJobEntry;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.database.DatabaseMeta;
+import org.pentaho.di.core.encryption.Encr;
 import org.pentaho.di.job.entries.ftpput.JobEntryFTPPUT;
 import org.pentaho.di.job.entry.JobEntryInterface;
-import org.pentaho.di.trans.steps.textfileoutput.TextFileOutputMeta;
 import org.pentaho.metastore.api.IMetaStore;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-
-import bsh.StringUtil;
 
 import com.mxgraph.model.mxCell;
 import com.mxgraph.util.mxUtils;
@@ -26,83 +23,59 @@ import com.mxgraph.util.mxUtils;
 public class JobEntryFTP_PUT extends AbstractJobEntry{
 	@Override
 	public void decode(JobEntryInterface jobEntry, mxCell cell, List<DatabaseMeta> databases, IMetaStore metaStore) throws Exception {
-		org.pentaho.di.job.entries.ftpput.JobEntryFTPPUT jobEntryFTPPUT = (org.pentaho.di.job.entries.ftpput.JobEntryFTPPUT) jobEntry;
-		//一般---服务器设置
-		jobEntryFTPPUT.setUserName(cell.getAttribute("username"));
-		jobEntryFTPPUT.setServerPort(cell.getAttribute("serverport"));
+	  JobEntryFTPPUT jobEntryFTPPUT = (JobEntryFTPPUT) jobEntry;
+	  jobEntryFTPPUT.setServerName(cell.getAttribute("serverName"));
+		jobEntryFTPPUT.setServerPort(cell.getAttribute("serverPort"));
+		jobEntryFTPPUT.setUserName(cell.getAttribute("userName"));
 		jobEntryFTPPUT.setPassword(cell.getAttribute("password"));
-		jobEntryFTPPUT.setServerName(cell.getAttribute("servername"));
-		jobEntryFTPPUT.setProxyHost(cell.getAttribute("proxyserver"));
-		jobEntryFTPPUT.setProxyPort(cell.getAttribute("proxyserverport"));
-		jobEntryFTPPUT.setProxyUsername(cell.getAttribute("proxyserverusername"));
-		jobEntryFTPPUT.setProxyPassword(cell.getAttribute("proxyserverpwd"));
-		
-		//一般---高级设置
-		jobEntryFTPPUT.setBinaryMode("Y".equals(cell.getAttribute("binarymode") ));
-		String timeoutString = cell.getAttribute("timeout");
-		if( !StringUtils.isEmpty(timeoutString) ){
-			boolean isDigital=false;
-			try{ Integer.parseInt( timeoutString ) ;isDigital=true;}catch(Exception e){}
-			 if( isDigital )
-				 jobEntryFTPPUT.setTimeout( Integer.parseInt( timeoutString ) );
-		}
-		jobEntryFTPPUT.setActiveConnection("Y".equals(cell.getAttribute("useraliveftpconnection")));
-		jobEntryFTPPUT.setControlEncoding(cell.getAttribute("controlencode"));
-		
-		//文件---源（本地）文件
-		jobEntryFTPPUT.setLocalDirectory(cell.getAttribute("localdir"));
-		jobEntryFTPPUT.setWildcard(cell.getAttribute("tongpeifu"));
-		jobEntryFTPPUT.setRemove("Y".equals(cell.getAttribute("dellocalfileafterupload")));
-		jobEntryFTPPUT.setOnlyPuttingNewFiles("Y".equals(cell.getAttribute("notcoverremotefiles")));
-		
-		//文件---目标（远程）文件
-		jobEntryFTPPUT.setRemoteDirectory(cell.getAttribute("remotedir"));
-		
-		//Sockets代理 ---代理
-		jobEntryFTPPUT.setProxyHost(cell.getAttribute("wProxy2server"));
-		jobEntryFTPPUT.setProxyPort(cell.getAttribute("wProxy2serverport"));
-		jobEntryFTPPUT.setProxyUsername(cell.getAttribute("proxy2serverusername"));
-		jobEntryFTPPUT.setProxyPassword(cell.getAttribute("proxy2serverpwd"));
-
+		jobEntryFTPPUT.setBinaryMode("Y".equalsIgnoreCase(cell.getAttribute("binaryMode")));
+		jobEntryFTPPUT.setTimeout(Const.toInt(cell.getAttribute("timeout"),0));
+		jobEntryFTPPUT.setActiveConnection("Y".equalsIgnoreCase(cell.getAttribute("activeConnection")));
+		jobEntryFTPPUT.setControlEncoding(cell.getAttribute("control_encoding"));
+		jobEntryFTPPUT.setProxyHost(cell.getAttribute("proxy_host"));
+		jobEntryFTPPUT.setProxyPort(cell.getAttribute("proxy_port"));
+		jobEntryFTPPUT.setProxyUsername(cell.getAttribute("proxy_username"));
+		jobEntryFTPPUT.setProxyPassword(Encr.decryptPasswordOptionallyEncrypted(cell.getAttribute("proxy_password")));
+		jobEntryFTPPUT.setLocalDirectory(cell.getAttribute("localDirectory"));
+		jobEntryFTPPUT.setWildcard(cell.getAttribute("wildcard"));
+		jobEntryFTPPUT.setRemove("Y".equalsIgnoreCase(cell.getAttribute("remove")));
+		jobEntryFTPPUT.setOnlyPuttingNewFiles("Y".equalsIgnoreCase(cell.getAttribute("only_new")));
+		jobEntryFTPPUT.setRemoteDirectory(cell.getAttribute("remoteDirectory"));
+		jobEntryFTPPUT.setSocksProxyHost(cell.getAttribute("socksproxy_host"));
+		jobEntryFTPPUT.setSocksProxyPort(cell.getAttribute("socksproxy_port"));
+		jobEntryFTPPUT.setSocksProxyUsername(cell.getAttribute("socksproxy_username"));
+		jobEntryFTPPUT.setSocksProxyPassword(cell.getAttribute("socksproxy_password"));
 	}
 
 	@Override
 	public Element encode(JobEntryInterface jobEntry) throws Exception {
 		JobEntryFTPPUT jobEntryFTPPUT = (JobEntryFTPPUT) jobEntry;
-
 		Document doc = mxUtils.createDocument();
 		Element e = doc.createElement(PropsUI.JOB_JOBENTRY_NAME);
-		//一般---服务器设置
-		e.setAttribute("servername", jobEntryFTPPUT.getServerName());
-		e.setAttribute("serverport", jobEntryFTPPUT.getServerPort());
-		e.setAttribute("username", jobEntryFTPPUT.getUserName());
-		e.setAttribute("password", jobEntryFTPPUT.getPassword());
-		e.setAttribute("proxyserver", jobEntryFTPPUT.getProxyHost());
-		e.setAttribute("proxyserverport", jobEntryFTPPUT.getProxyPort());
-		e.setAttribute("proxyserverusername", jobEntryFTPPUT.getProxyUsername());
-		e.setAttribute("proxyserverpwd", jobEntryFTPPUT.getProxyPassword());
-
-		//一般----高级设置
-		e.setAttribute("binarymode", jobEntryFTPPUT.isBinaryMode() ?"Y":"N");
-		e.setAttribute("timeout", jobEntryFTPPUT.getTimeout()+"");
-		e.setAttribute("usealiveftpconnection",  jobEntryFTPPUT.isActiveConnection() ?"Y":"N");
-		e.setAttribute("contrlEncode",jobEntryFTPPUT.getControlEncoding() );
-
-		//文件---源（本地）文件
-		e.setAttribute("localdir", jobEntryFTPPUT.getLocalDirectory());
-		e.setAttribute("tongpeifu", jobEntryFTPPUT.getWildcard());
-		e.setAttribute("dellocalfilesafterupload", jobEntryFTPPUT.getRemove()?"Y":"N");
-		e.setAttribute("notcoverremotefiles", jobEntryFTPPUT.isOnlyPuttingNewFiles()?"Y":"N");
 		
-		//文件---目标（远程）文件
-		e.setAttribute("remotedir", jobEntryFTPPUT.getRemoteDirectory());
-		
-		//Sockets代理---代理
-		e.setAttribute("proxy2server", jobEntryFTPPUT.getProxyHost());
-		e.setAttribute("proxy2serverport", jobEntryFTPPUT.getProxyPort());
-		e.setAttribute("proxy2serverusername", jobEntryFTPPUT.getProxyUsername());
-		e.setAttribute("proxy2serverpwd", jobEntryFTPPUT.getProxyPassword());
-		
+		e.setAttribute("serverName", jobEntryFTPPUT.getServerName());
+		e.setAttribute("serverPort", jobEntryFTPPUT.getServerPort());
+		e.setAttribute("userName", jobEntryFTPPUT.getUserName());
+		e.setAttribute("password", jobEntryFTPPUT.getPassword() );
+		e.setAttribute("binaryMode", jobEntryFTPPUT.isBinaryMode()? "Y" : "N" );
+		e.setAttribute("timeout", jobEntryFTPPUT.getTimeout()+ "");
+		e.setAttribute("activeConnection", jobEntryFTPPUT.isActiveConnection()? "Y" : "N");
+		e.setAttribute("control_encoding", jobEntryFTPPUT.getControlEncoding());
+		e.setAttribute("proxy_host", jobEntryFTPPUT.getProxyHost());
+		e.setAttribute("proxy_port", jobEntryFTPPUT.getProxyPort());
+		e.setAttribute("proxy_username", jobEntryFTPPUT.getProxyUsername());
+		e.setAttribute("proxy_password", jobEntryFTPPUT.getProxyPassword());
+		e.setAttribute("localDirectory", jobEntryFTPPUT.getLocalDirectory());
+		e.setAttribute("wildcard", jobEntryFTPPUT.getWildcard());
+		e.setAttribute("remove", jobEntryFTPPUT.getRemove() ? "Y" : "N");
+		e.setAttribute("only_new", jobEntryFTPPUT.isOnlyPuttingNewFiles()? "Y" : "N");
+		e.setAttribute("wildcard", jobEntryFTPPUT.getWildcard());
+		e.setAttribute("remove", jobEntryFTPPUT.getRemove() ? "Y" : "N");
+		e.setAttribute("remoteDirectory", jobEntryFTPPUT.getRemoteDirectory());
+		e.setAttribute("socksproxy_host", jobEntryFTPPUT.getSocksProxyHost());
+		e.setAttribute("socksproxy_port", jobEntryFTPPUT.getSocksProxyPort());
+		e.setAttribute("socksproxy_username", jobEntryFTPPUT.getSocksProxyUsername());
+		e.setAttribute("socksproxy_password", jobEntryFTPPUT.getSocksProxyPassword());
 		return e;
 	}
 
